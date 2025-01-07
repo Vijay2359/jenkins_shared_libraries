162c494915bda72acf321cf9344e883f47e59b97
Vijay2359 <vijaysinh.gaikwad@cognologix.com>
2025-01-07 15:58:22 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,4 +1,4 @@
-def call( String url,String branch){
- echo 'Cloning the repo'
-                git "${url}": url, branch: "${branch}"
+def call(String url, String branch){
+ 
+   git url: "${url}", branch: "${branch}"
 }
